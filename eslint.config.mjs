aad14285a7489c7b0659b84b1f472860830ef0d7
Yash Sharma <86138890+Yashkr00129@users.noreply.github.com>
2026-01-09 00:26:34 +0530

    Added necessary components and pages.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,23 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	...nextTs,
+	// Override default ignores of eslint-config-next.
+	{
+		rules: {
+			"@next/next/no-img-element": "off",
+			"react/no-unescaped-entities": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	},
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+	]),
 ]);
 
 export default eslintConfig;
