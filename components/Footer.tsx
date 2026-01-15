2a834edf75471ff2945186f48266754f95344ea3
Manish Gupta <guptamanish3239@gmail.com>
2026-01-15 15:32:37 +0530

    added space

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -78,7 +78,7 @@ export default function Footer() {
 		<footer className="bg-white dark:bg-gray-900">
 			<div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
 				<p className="mt-8 text-center text-sm/6 text-gray-600 md:order-1 md:mt-0 dark:text-gray-400">
-					&copy; {currentYear} Portfolio. All rights reserved.
+					&copy; {currentYear} Portfolio. All rights  reserved.
 				</p>
 				<div className="flex justify-center gap-x-6 md:order-2">
 					{socialLinks.map((item) => (
